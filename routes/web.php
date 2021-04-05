452326c02fe20543fa09612fe197b911710fdd15
Asklat <alejandro.gamaza.martinez.vega@gmail.com>
2021-04-05 19:54:51 +0200

    prueba de git

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
