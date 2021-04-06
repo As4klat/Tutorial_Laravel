7c24ac20850f9867d30a4777bbef549289bb8c6f
Asklat <alejandro.gamaza.martinez.vega@gmail.com>
2021-04-06 17:58:02 +0200

    Emails

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,4 +4,19 @@
 
 @section('content')
     <h1>Esto es el Contact</h1>
+
+    <form method="POST" action="{{route('contact')}}">
+
+        @csrf
+
+        <input type="text" name="name" placeholder="Nombre ..."><br>
+        {!! $errors->first('name', '<small>:message</small><br>') !!}
+        <input type="email" name="email" placeholder="Email ..."><br>
+        {!! $errors->first('email', '<small>:message</small><br>') !!}
+        <input type="subject" name="asunto" placeholder="Asunto ..."><br>
+        {!! $errors->first('asunto', '<small>:message</small><br>') !!}
+        <textarea name="contenido" id="" cols="38" rows="1"></textarea><br>
+        {!! $errors->first('contenido', '<small>:message</small><br>') !!}
+        <button>Enviar</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 Route::get('/portafolio', 'PortafolioController@index')->name('portafolio');
+
+Route::post('contact', 'MessagesController@store');
